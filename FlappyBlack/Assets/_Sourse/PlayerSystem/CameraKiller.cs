816f7ef3bf6818469db94cb10b063aad70e671ef
ITHUB\Student <qew>
2024-12-12 19:44:29 +0300

    descriptions
    + description

--- FlappyBlack/Assets/_Sourse/PlayerSystem/CameraKiller.cs
+++ FlappyBlack/Assets/_Sourse/PlayerSystem/CameraKiller.cs
@@ -1,25 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using PlayerSystem;
 
 namespace PlayerSystem
 {
     public class CameraKiller : MonoBehaviour
     {
-        private string sceneName = "Menu";
         CameraMove cameramove;
         EndSetActive EndSetActive;
         void OnBecameInvisible()
         {
             if (gameObject.CompareTag("Player"))
             {
-
-                CameraMove.PlayerDeath = true;
-                EndSetActive.CheckAndActivateObject(true);
-                Destroy(gameObject);
-                
+                ActiveteDeath();
+                SceneManager.LoadScene("Menu");
             }
+        }
+        private void ActiveteDeath()
+        {
+            CameraMove.PlayerDeath = true;
+            Destroy(gameObject);
+            EndSetActive.CheckAndActivateObject(true);
+            //Destroy(gameObject); не умирает если тут 
+
         }
     }
 
